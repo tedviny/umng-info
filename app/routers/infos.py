2cde06798da92919196db794d7158f205636488c
Ted Loungou <86886512+tedviny@users.noreply.github.com>
2023-07-02 21:02:23 +0200

    units tests added (#1)

--- app/routers/infos.py
+++ app/routers/infos.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from views.umng_info import umng_general_info, add_info, update_info, delete_info
-from models.umng_info import Info
+from app.views.umng_info import umng_general_info, add_info, update_info, delete_info
+from app.models.umng_info import Info
 
 router = APIRouter(prefix="/api/v1/infos",)
 
@@ -13,10 +13,12 @@ def add_infos(info: Info):
     add_info(info)
     return "Information added"
 
+
 @router.put("/{id}", tags=["Update information"])
 def update_infos(id: str, info: Info):
     update_info(id,info)
     return "Information successfully updated"
+
 
 @router.delete("/{id}", tags=["Delete information"])
 def delete_infos(id: str):
